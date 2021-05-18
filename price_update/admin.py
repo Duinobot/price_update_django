d7716a3d4752e9176439ef37e23fac7bbb8a8690
dan <danmo2060@gmail.com>
2021-05-18 18:19:07 +1000

    added product model

--- price_update/admin.py
+++ price_update/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from price_update.models import Product
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost', 'model', 'product_type')
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
--- price_update/models.py
+++ price_update/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import BooleanField, CharField
 
 # Create your models here.
+
+
+class Product(models.Model):
+
+    class ProductType(models.IntegerChoices):
+        Accessories = 1
+        Backs = 2
+        Batteries = 3
+        Cables = 4
+        Cases = 5
+        Parts = 6
+        Phones = 7
+        Screens = 8
+        Tempered_Glasses = 9
+        Tools = 10
+        Used = 11
+
+    model = models.IntegerField(blank=False, verbose_name="Model ID")
+    name = models.CharField(blank=False, max_length=100,
+                            verbose_name="Product Name")
+    cost = models.DecimalField(
+        blank=True, max_digits=6, decimal_places=2, verbose_name="Cost")
+    price_regular = models.DecimalField(
+        blank=True, max_digits=6, decimal_places=2)
+    price_platinum = models.DecimalField(
+        blank=True, max_digits=6, decimal_places=2)
+    price_vvip = models.DecimalField(
+        blank=True, max_digits=6, decimal_places=2)
+    price_wholesale = models.DecimalField(
+        blank=True, max_digits=6, decimal_places=2)
+    product_type = models.IntegerField(
+        choices=ProductType.choices, verbose_name="Type")
+    own_formula_regular = models.CharField(blank=True, max_length=100)
+    own_formula_platinum = models.CharField(blank=True, max_length=100)
+    own_formula_vvip = models.CharField(blank=True, max_length=100)
+    own_formula_wholesale = models.CharField(blank=True, max_length=100)
+    use_own_formula = models.BooleanField(default=False)
