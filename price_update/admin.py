2fabbe6f45eadcd31b45715722c6135c5f44ec32
dan <danmo2060@gmail.com>
2021-05-21 18:15:53 +1000

    try to add csv import

--- price_update/admin.py
+++ price_update/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django import forms
+from django.forms.fields import FileField
+from django.views.generic.base import RedirectView
 from price_update.models import Product
+from django.urls import path
 
 
-# Register your models here.
+class CsvImportForm(forms.Form):
+    csv_file = FileField()
+
+
 class ProductAdmin(admin.ModelAdmin):
 
     # Add Calculated field Change % in product detail field
@@ -29,7 +36,31 @@ class ProductAdmin(admin.ModelAdmin):
         ('Formula', {'fields': [
          'use_own_formula', 'own_formula_regular', 'own_formula_platinum', 'own_formula_vvip', 'own_formula_wholesale']}),
     ]
+
+    # Add csv upload function
+    change_list_template = 'price_update/product_changelist.html'
+
+    def get_url(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_csv),
+        ]
+        return my_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES['csv_file']
+            reader = csv.reader(csv_file)
+            self.message_user(request, "Your csv file has been imported")
+            return redirect("..")
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(
+            request, "price_update/csv_form.html", payload
+        )
+
     pass
 
 
+# Register your models here.
 admin.site.register(Product, ProductAdmin)
