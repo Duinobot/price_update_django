d79821d7700887f452e2bbf2ffcaa322833288b4
dan <danmo2060@gmail.com>
2021-05-27 18:29:00 +1000

    add product list view

--- price_update/urls.py
+++ price_update/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
 from . import views
 
-
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('product', views.ProductPageView.as_view(), name='product'),
 ]
--- price_update/views.py
+++ price_update/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ProductSerializer
 from .models import Product
+from django.views.generic import ListView
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('model')
     serializer_class = ProductSerializer
+
+
+class ProductPageView(ListView):
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'all_product_list'
